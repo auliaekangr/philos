6571e6e348600229f35e5f9aa29c839caf43dfb7
aul <aulia20205@gmail.com>
2021-09-08 20:15:07 +0700

    ganti font quicksan

--- page/Chat-bot/speech.js
+++ page/Chat-bot/speech.js
@@ -5,7 +5,7 @@ const synth = window.speechSynthesis;
 const textToSpeech = (string) => {
   let voice = new SpeechSynthesisUtterance(string);
   voice.text = string;
-  voice.lang = "en-US";
+  voice.lang = "id-ID";
   voice.volume = 1;
   voice.rate = 1;
   voice.pitch = 1; // Can be 0, 1, or 2
